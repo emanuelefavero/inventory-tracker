ba1983c09cffa2dfb384126965c379f14023a2c7
Emanuele Favero <emafaux89@gmail.com>
2025-12-06 01:51:53 +0100

    feat: update css custom properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { Trash2 } from 'lucide-react'
+
 export default function Home() {
   return (
     <>
       <h1 className='text-4xl font-bold'>Hello</h1>
+
+      <div className='mt-2 flex flex-wrap gap-2'>
+        <Button variant='outline'>Outline Button</Button>
+        <Button className='align-bottom'>Default Button</Button>
+        <Button variant='secondary'>Secondary Button</Button>
+        <Button variant='ghost'>Ghost Button</Button>
+        <Button variant='link'>Link Button</Button>
+        <Button variant='destructive'>Destructive Button</Button>
+        <Button variant='ghost'>
+          <Trash2 />
+        </Button>
+        <Button variant='destructive'>
+          <Trash2 />
+        </Button>
+        <input
+          type='text'
+          placeholder='Ciao'
+          className='rounded-lg border border-border bg-input px-2'
+        />
+      </div>
     </>
   )
 }
