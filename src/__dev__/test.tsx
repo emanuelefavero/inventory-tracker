92dd4dcb35dd7558099a7c93491d920b7be636cc
Emanuele Favero <emafaux89@gmail.com>
2026-01-11 01:06:48 +0100

    feat: move buttons from home page to test component

--- src/__dev__/test.tsx
+++ src/__dev__/test.tsx
@@ -1,11 +1,34 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { useIsMounted } from '@/hooks/use-is-mounted'
+import { Trash2 } from 'lucide-react'
 
 export function Test() {
   const isMounted = useIsMounted()
 
   if (!isMounted) return null
 
-  return <div>Hello from test file</div>
+  return (
+    <div className='mt-4 flex flex-wrap gap-2 rounded-2xl border border-foreground/20 p-4'>
+      <Button variant='outline'>Outline Button</Button>
+      <Button className='align-bottom'>Default Button</Button>
+      <Button variant='secondary'>Secondary Button</Button>
+      <Button variant='ghost'>Ghost Button</Button>
+      <Button variant='link'>Link Button</Button>
+      <Button variant='destructive'>Destructive Button</Button>
+      <Button variant='ghost'>
+        <Trash2 />
+      </Button>
+      <Button variant='destructive'>
+        <Trash2 />
+      </Button>
+      <input
+        type='text'
+        placeholder='Ciao'
+        className='rounded-lg border border-border bg-input px-2'
+      />
+      <a href='#'>Test Link</a>
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { Test } from '@/__dev__/test'
-import { Button } from '@/components/ui/button'
-import { Trash2 } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -8,27 +6,6 @@ export default function Home() {
       <h1 className='text-4xl font-bold'>Hello</h1>
 
       {process.env.NODE_ENV === 'development' && <Test />}
-
-      <div className='mt-2 flex flex-wrap gap-2'>
-        <Button variant='outline'>Outline Button</Button>
-        <Button className='align-bottom'>Default Button</Button>
-        <Button variant='secondary'>Secondary Button</Button>
-        <Button variant='ghost'>Ghost Button</Button>
-        <Button variant='link'>Link Button</Button>
-        <Button variant='destructive'>Destructive Button</Button>
-        <Button variant='ghost'>
-          <Trash2 />
-        </Button>
-        <Button variant='destructive'>
-          <Trash2 />
-        </Button>
-        <input
-          type='text'
-          placeholder='Ciao'
-          className='rounded-lg border border-border bg-input px-2'
-        />
-        <a href='#'>Test Link</a>
-      </div>
     </>
   )
 }
