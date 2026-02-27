4fdacc0951ec7e6ee33bdf657f619f02289a3a49
Emanuele Favero <emafaux89@gmail.com>
2026-02-27 08:15:00 +0100

    build: update clerk auth, fix clerk suspense error

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,19 +6,22 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs'
+import { Suspense } from 'react'
 
 export function Header() {
   return (
     <header className='flex items-center justify-between gap-2 border-b border-border px-px py-py'>
       <div className='font-bold'>Inventory Tracker</div>
       <div className='flex items-center gap-4'>
-        <SignedOut>
-          <SignInButton mode='modal' />
-          <SignUpButton mode='modal' />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <Suspense>
+          <SignedOut>
+            <SignInButton mode='modal' />
+            <SignUpButton mode='modal' />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Suspense>
         <ModeToggle />
       </div>
     </header>
